332a66a37ddd45b4d8454cb37dbe71d6d9611126
hideokuda <okuda.hidehito@gmail.com>
2023-03-30 19:46:46 +0900

    competition 0.5

--- game_manager/block_controller.py
+++ game_manager/block_controller.py
@@ -33,6 +33,7 @@ class Block_Controller(object):
         #       False : Normal Mode
         #
         FourLineStrategy = False
+        UseHoldFunction = True
 
         # print GameStatus
         print("=================================================>")
@@ -52,52 +53,175 @@ class Block_Controller(object):
         self.board_data_width = GameStatus["field_info"]["width"]
         self.board_data_height = GameStatus["field_info"]["height"]
         self.ShapeNone_index = GameStatus["debug_info"]["shape_info"]["shapeNone"]["index"]
+        # hold shape info
+        HoldShapeDirectionRange = GameStatus["block_info"]["holdShape"]["direction_range"]
+        HoldShapeIndex = GameStatus["block_info"]["holdShape"]["index"]
+        HoldShape_class = GameStatus["block_info"]["holdShape"]["class"]
+        # temporary shape info
+        tempShapeDirectionRange = CurrentShapeDirectionRange
+        tempShapeIndex = CurrentShapeIndex
+        tempShape_class = self.CurrentShape_class
+
+        # 最初に Hold をした(Holdが空だったか)を示すフラグ
+        FirstHoldExecution = False
+
+        # がっつりモードで Hold 機能有効の時だけ、
+        # Iミノがきれいに収まりそうな時(Iミノを積極的に使う):
+        #   Hold のミノが I の時:
+        #     CurrentShape が I ミノじゃないとき:
+        #       I ミノを Hold から出してくる
+        #     CurrentShape が I ミノの時：
+        #       Hold ミノはそのまま Hold しておいて CurrentShape の Iミノを使う
+        #   Hold ミノが I以外 (I以外のミノか、Hold が無い場合)：
+        #     どうしようもないのでそのまま
+        # がっつり埋まりそうじゃないとき(IミノをできるだけHoldしておく)
+        #   CurrentShape が I の時：
+        #     Hold が I 以外の時：
+        #       IをHoldする
+        #     Hold が I の時：
+        #       もう I が Hold されてるのでそのまま
+        #   CurrentShape が Ｉ じゃないとき：
+        #     当然ながらそのまま
+
+        # I ミノが縦に収まりそうな空きがあるかチェックする
+        # 時間がかかるかどうかチェックしたいのでモードに関わらずチェックするようにする
+        # ほぼ calcEvaluationValueSample からのコピペで恥ずかしいけど我慢
+        width = self.board_data_width
+        height = self.board_data_height
+        ## まずは各列の高さを求める
+        BlockMaxY = [0] * width
+        ### check back board (ミノを置く前のボード)
+        # each y line
+        for y in range(height - 1, 0, -1):
+            hasHole = False
+            hasBlock = False
+            # each x line
+            for x in range(width):
+                ## check if hole or block..
+                if self.board_backboard[y * self.board_data_width + x] != self.ShapeNone_index:
+                    BlockMaxY[x] = height - y                # update blockMaxY
+
+        ### 各列の高さから、へこみがあるかチェック
+        highGap = 3 # Gap があると認識する高さの差
+        onlyOneColumnSpace = False  # 1列だけの空きがあるかどうかを示すフラグ
+        lefthight = height # 注目列の左列の高さ。列0は左端なので最大の高さを入れておく
+        switchHold = 'n' # Hold をswichするかどうかのフラグ
+        for i in range(width - 1):
+            if lefthight - BlockMaxY[i] >= highGap:
+                # へこんでる
+                if i == width - 1: # 右端だったら I ミノが縦に収まる
+                    onlyOneColumnSpace = True
+                else:
+                    if BlockMaxY[i+1] -  BlockMaxY[i] >= highGap:
+                        # すぐまた立ち上がっている時は I ミノが縦に収まる
+                        onlyOneColumnSpace = True
+            else:
+                # へこんでないときは次の列チェックにそなえて比較用の左列高さにいまの高さを入れる
+                lefthight = BlockMaxY[i]
+        #
+        if UseHoldFunction == True:
+            if onlyOneColumnSpace == True:
+                if HoldShapeIndex == 1:
+                    if CurrentShapeIndex != 1:
+                        # Hold から I ミノを出す
+                        switchHold = 'y'
+                        tempShapeDirectionRange = CurrentShapeDirectionRange
+                        tempShapeIndex = CurrentShapeIndex
+                        tempShape_class = self.CurrentShape_class
+                        CurrentShapeDirectionRange = HoldShapeDirectionRange
+                        CurrentShapeIndex = HoldShapeIndex
+                        self.CurrentShape_class = HoldShape_class
+                        HoldShapeDirectionRange = tempShapeDirectionRange
+                        HoldShapeIndex = tempShapeIndex
+                        HoldShape_class = tempShape_class
+                    else:
+                        pass
+                else:
+                    if HoldShapeIndex == None:
+                        FirstHoldExecution = True
+                    else:
+                        pass
+            else:
+                if CurrentShapeIndex == 1:
+                    if HoldShapeIndex != 1:
+                        if HoldShapeIndex == None:
+                            FirstHoldExecution = True
+                        #  I ミノ を Holdに入れる
+                        switchHold = 'y'
+                        tempShapeDirectionRange = CurrentShapeDirectionRange
+                        tempShapeIndex = CurrentShapeIndex
+                        tempShape_class = self.CurrentShape_class
+                        CurrentShapeDirectionRange = HoldShapeDirectionRange
+                        CurrentShapeIndex = HoldShapeIndex
+                        self.CurrentShape_class = HoldShape_class
+                        HoldShapeDirectionRange = tempShapeDirectionRange
+                        HoldShapeIndex = tempShapeIndex
+                        HoldShape_class = tempShape_class
+                    else:
+                        pass # IミノHold 済のため、そのまま
+                else:
+                    pass
 
         # search best nextMove -->
         strategy = None
         LatestEvalValue = -100000
-        # number of holes before putting tetrimino
-        RetArray = self.calcEvaluationValueSample(self.board_backboard, 0)
-        Holes_wo_NewTetrimino = RetArray[1]
 
-        # search with current block Shape
-        for direction0 in CurrentShapeDirectionRange:
-            # search with x range
-            x0Min, x0Max = self.getSearchXRange(self.CurrentShape_class, direction0)
-            for x0 in range(x0Min, x0Max):
-                # get board data, as if dropdown block
-                board = self.getBoard(self.board_backboard, self.CurrentShape_class, direction0, x0)
+        if FirstHoldExecution == False:
+            # number of holes before putting tetrimino
+            RetArray = self.calcEvaluationValueSample(self.board_backboard, 0)
+            Holes_wo_NewTetrimino = RetArray[1]
 
-                # evaluate board
-                RetArray = self.calcEvaluationValueSample(board, Holes_wo_NewTetrimino)
-                EvalValue = RetArray[0]
-                print(x0, x0Max-1, CurrentShapeIndex, direction0)
-                if FourLineStrategy and x0 == (x0Max-1) and not (CurrentShapeIndex == 1 and direction0 == 0):
-                    EvalValue = EvalValue - 100
-                    print ("Hello")
-                    print(EvalValue)
-                if EvalValue > LatestEvalValue:
-                    strategy = (direction0, x0, 1, 1)
-                    LatestEvalValue = EvalValue
+            # search with current block Shape
+            for direction0 in CurrentShapeDirectionRange:
+                # search with x range
+                x0Min, x0Max = self.getSearchXRange(self.CurrentShape_class, direction0)
+                for x0 in range(x0Min, x0Max):
+                    # get board data, as if dropdown block
+                    board = self.getBoard(self.board_backboard, self.CurrentShape_class, direction0, x0)
 
-                ###test
-                ###for direction1 in NextShapeDirectionRange:
-                ###  x1Min, x1Max = self.getSearchXRange(self.NextShape_class, direction1)
-                ###  for x1 in range(x1Min, x1Max):
-                ###        board2 = self.getBoard(board, self.NextShape_class, direction1, x1)
-                ###        EvalValue = self.calcEvaluationValueSample(board2)
-                ###        if EvalValue > LatestEvalValue:
-                ###            strategy = (direction0, x0, 1, 1)
-                ###            LatestEvalValue = EvalValue
-        # search best nextMove <--
+                    # evaluate board
+                    RetArray = self.calcEvaluationValueSample(board, Holes_wo_NewTetrimino)
+                    EvalValue = RetArray[0]
+                    print(x0, x0Max-1, CurrentShapeIndex, direction0)
+                    if FourLineStrategy and x0 == (x0Max-1) and not (CurrentShapeIndex == 1 and direction0 == 0):
+                        EvalValue = EvalValue - 100
+                        print ("Hello")
+                        print(EvalValue)
+                    if EvalValue > LatestEvalValue:
+                        strategy = (direction0, x0, 1, 1, 'n')
+                        # Hold機能使用時に再利用するために保存しておく
+                        savdirection0 = direction0
+                        savx0 = x0
+                        LatestEvalValue = EvalValue
+            
+                    ###test
+                    ###for direction1 in NextShapeDirectionRange:
+                    ###  x1Min, x1Max = self.getSearchXRange(self.NextShape_class, direction1)
+                    ###  for x1 in range(x1Min, x1Max):
+                    ###        board2 = self.getBoard(board, self.NextShape_class, direction1, x1)
+                    ###        EvalValue = self.calcEvaluationValueSample(board2)
+                    ###        if EvalValue > LatestEvalValue:
+                    ###            strategy = (direction0, x0, 1, 1)
+                    ###            LatestEvalValue = EvalValue
+            # search best nextMove <--
+
+            nextMove["strategy"]["direction"] = strategy[0]
+            nextMove["strategy"]["x"] = strategy[1]
+            nextMove["strategy"]["y_operation"] = strategy[2]
+            nextMove["strategy"]["y_moveblocknum"] = strategy[3]
+            nextMove["strategy"]["use_hold_function"] = switchHold
+        else:
+            nextMove["strategy"]["direction"] = 0
+            nextMove["strategy"]["x"] = 0
+            nextMove["strategy"]["y_operation"] = 0
+            nextMove["strategy"]["y_moveblocknum"] = 0
+            nextMove["strategy"]["use_hold_function"] = switchHold
 
         print("===", datetime.now() - t1)
-        nextMove["strategy"]["direction"] = strategy[0]
-        nextMove["strategy"]["x"] = strategy[1]
-        nextMove["strategy"]["y_operation"] = strategy[2]
-        nextMove["strategy"]["y_moveblocknum"] = strategy[3]
         print(nextMove)
         print("###### SAMPLE CODE ######")
+            
+        
         return nextMove
 
     def getSearchXRange(self, Shape_class, direction):
@@ -244,7 +368,7 @@ class Block_Controller(object):
         score = score - nHoles * 3.0               # try not to make hole
         score = score - nIsolatedBlocks * 3.0      # try not to make isolated block
         score = score - absDy * 2.0                # try to put block smoothly
-        score = score - holesDiff_w_wo_tetrimino * 20.0 # try 
+        score = score - holesDiff_w_wo_tetrimino * 20.0 # try to reduce new holes 
 
         #score = 0
         #score = score + fullLines * 10.0           # try to delete line 
